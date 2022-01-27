f191031fcce65c3f2ea712972669cde267951cb8
Ary-Cardoso <98437304+Ary-Cardoso@users.noreply.github.com>
2022-01-27 16:23:02 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,47 @@
+
+const entradaDeTexto = document.querySelector('.entradaDeTexto input');
+
+const botaoCriptografar = document.querySelector('.entradaDeTexto button');
+
+const botaoDescriptografar = document.getElementById('descript');
+  
+const saidaDeTexto = document.querySelector('.saidaDeTexto input');
+
+const botaoCopiar = document.querySelector('.saidaDeTexto button');
+
+botaoCriptografar.addEventListener('click', criptografarTexto);
+botaoDescriptografar.addEventListener('click', descriptografarTexto);
+
+
+
+function criptografarTexto() {
+
+    var capturar = entradaDeTexto.value;              
+    criptografar = capturar.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');         
+    saidaDeTexto.value = criptografar;
+    entradaDeTexto.value = "";
+}
+
+
+function descriptografarTexto() {
+    var capturar = entradaDeTexto.value;              
+    descriptografar = capturar.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');         
+    saidaDeTexto.value = descriptografar;
+    
+} 
+
+botaoCopiar.addEventListener('click', function copiar() {
+    navigator.clipboard.writeText(document.querySelector('.saidaDeTexto input').value)
+
+});
+
+
+
+
+           
+                       
+    
+        
+
+
+
